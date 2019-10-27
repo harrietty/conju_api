0627b70803415365a20f1944ad15a3d410bfa763
Harriet Ryder <harriethryder@gmail.com>
2019-10-27 20:42:21 +0000

    Get object from s3

--- get_verbs/main.go
+++ get_verbs/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -13,6 +17,27 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	if !ok {
 		return events.APIGatewayProxyResponse{StatusCode: 404}, nil
 	}
+
+	svc := s3.New(session.New())
+	input := &s3.GetObjectInput{
+			Bucket: aws.String("conjugator-verb-data"),
+			Key:    aws.String(language + ".json"),
+	}
+	result, err := svc.GetObject(input)
+	if err != nil {
+    if aerr, ok := err.(awserr.Error); ok {
+        switch aerr.Code() {
+        case s3.ErrCodeNoSuchKey:
+            fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+        default:
+            fmt.Println(aerr.Error())
+        }
+    } else {
+        fmt.Println(err.Error())
+		}
+	}
+		
+	fmt.Println(result)
 
 	return events.APIGatewayProxyResponse{Body: "You requested " + language, StatusCode: 200}, nil
 }
