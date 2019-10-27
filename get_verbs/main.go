2bf22b5731edd9415b0de5208e279137a4c82512
Harriet Ryder <harriethryder@gmail.com>
2019-10-27 21:26:35 +0000

    Fetch and parse JSON from S3

--- get_verbs/main.go
+++ get_verbs/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"io/ioutil"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,7 +13,7 @@ import (
 )
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Received request: ", request.HTTPMethod, request.Path, request.QueryStringParameters)
+	log.Println("Received request: ", request.HTTPMethod, request.Path, request.QueryStringParameters)
 
 	language, ok := request.QueryStringParameters["language"]
 	if !ok {
@@ -28,18 +30,56 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
     if aerr, ok := err.(awserr.Error); ok {
         switch aerr.Code() {
         case s3.ErrCodeNoSuchKey:
-            fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+            log.Println(s3.ErrCodeNoSuchKey, aerr.Error())
         default:
-            fmt.Println(aerr.Error())
+            log.Println(aerr.Error())
         }
     } else {
-        fmt.Println(err.Error())
+        log.Println(err.Error())
 		}
+
+		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
 	}
 		
-	fmt.Println(result)
+	s3ObjectBytes, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		log.Println("Error reading S3 result body: ", err)
+	}
+
+	languageData := parseLanguageJson(s3ObjectBytes)
+	log.Println(languageData)
 
 	return events.APIGatewayProxyResponse{Body: "You requested " + language, StatusCode: 200}, nil
+}
+
+type LanguageData struct {
+	Pronouns []string `json:"pronouns"`
+	Verbs Verbs `json:"verbs"`
+}
+
+type Verbs struct {
+	Basic []Verb `json:"basic"`
+}
+
+type Verb struct {
+	Infinitive string `json:"infinitive"`
+	Translations []string `json:"translations"`
+	Type []string `json:"type"`
+	Conjugations Conjugations `json:"conjugations"`
+}
+
+type Conjugations struct {
+	Present []string `json:"present"`
+	Preterite []string `json:"preterite"`
+	Imperfect []string `json:"imperfect"`
+	Conditional []string `json:"conditional"`
+	Future []string `json:"future"`
+}
+
+func parseLanguageJson(jsonData []byte) LanguageData {
+	var langData LanguageData
+	json.Unmarshal(jsonData, &langData)
+	return langData
 }
 
 func main() {
