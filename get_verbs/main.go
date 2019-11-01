58181de2a3d288a22e55203a58ada35d951003c8
Harriet Ryder <harriethryder@gmail.com>
2019-11-01 18:33:51 +0000

    Responds with infinitive verbs in specified language

--- get_verbs/main.go
+++ get_verbs/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"log"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("Received request: ", request.HTTPMethod, request.Path, request.QueryStringParameters)
 
 	language, ok := request.QueryStringParameters["language"]
@@ -22,66 +23,80 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	svc := s3.New(session.New())
 	input := &s3.GetObjectInput{
-			Bucket: aws.String("conjugator-verb-data"),
-			Key:    aws.String(language + ".json"),
+		Bucket: aws.String("conjugator-verb-data"),
+		Key:    aws.String(language + ".json"),
 	}
 	result, err := svc.GetObject(input)
 	if err != nil {
-    if aerr, ok := err.(awserr.Error); ok {
-        switch aerr.Code() {
-        case s3.ErrCodeNoSuchKey:
-            log.Println(s3.ErrCodeNoSuchKey, aerr.Error())
-        default:
-            log.Println(aerr.Error())
-        }
-    } else {
-        log.Println(err.Error())
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchKey:
+				log.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+			default:
+				log.Println(aerr.Error())
+			}
+		} else {
+			log.Println(err.Error())
 		}
 
 		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
 	}
-		
+
 	s3ObjectBytes, err := ioutil.ReadAll(result.Body)
 	if err != nil {
 		log.Println("Error reading S3 result body: ", err)
 	}
 
-	languageData := parseLanguageJson(s3ObjectBytes)
-	log.Println(languageData)
+	langData := parseLanguageJSON(s3ObjectBytes)
+	// langDataJSON, err := json.Marshal(langData)
+	inf, err := json.Marshal(extractInfinitives(langData))
+	if err != nil {
+		log.Println("Error marshalling JSON: ", err)
+	}
 
-	return events.APIGatewayProxyResponse{Body: "You requested " + language, StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{Body: string(inf), StatusCode: 200}, nil
 }
 
-type LanguageData struct {
+type infinitives []string
+
+type languageData struct {
 	Pronouns []string `json:"pronouns"`
-	Verbs Verbs `json:"verbs"`
+	Verbs    verbs    `json:"verbs"`
 }
 
-type Verbs struct {
-	Basic []Verb `json:"basic"`
+type verbs struct {
+	Basic []verb `json:"basic"`
 }
 
-type Verb struct {
-	Infinitive string `json:"infinitive"`
-	Translations []string `json:"translations"`
-	Type []string `json:"type"`
-	Conjugations Conjugations `json:"conjugations"`
+type verb struct {
+	Infinitive   string       `json:"infinitive"`
+	Translations []string     `json:"translations"`
+	Type         []string     `json:"type"`
+	Conjugations conjugations `json:"conjugations"`
 }
 
-type Conjugations struct {
-	Present []string `json:"present"`
-	Preterite []string `json:"preterite"`
-	Imperfect []string `json:"imperfect"`
+type conjugations struct {
+	Present     []string `json:"present"`
+	Preterite   []string `json:"preterite"`
+	Imperfect   []string `json:"imperfect"`
 	Conditional []string `json:"conditional"`
-	Future []string `json:"future"`
+	Future      []string `json:"future"`
 }
 
-func parseLanguageJson(jsonData []byte) LanguageData {
-	var langData LanguageData
+func parseLanguageJSON(jsonData []byte) languageData {
+	var langData languageData
 	json.Unmarshal(jsonData, &langData)
 	return langData
 }
 
+func extractInfinitives(languageData languageData) []string {
+	var res []string
+	for _, val := range languageData.Verbs.Basic {
+		res = append(res, val.Infinitive)
+	}
+	return res
+}
+
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(handler)
 }
